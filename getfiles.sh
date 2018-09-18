dee6399a70632b355729ce28492c3f341fd6cc2a
Chaos Shen <chaosdefinition@hotmail.com>
2018-09-18 00:04:13 -0400

    Removed downloading and unpacking QEMU files.

--- getfiles.sh
+++ getfiles.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-echo "Fetching QEMU"
-wget http://www.cs.rochester.edu/courses/256/fall2015/QDGL/qemu-install.tar.gz
-
 echo "Fetching OpenWRT Hard Disk Image"
-wget http://www.cs.rochester.edu/courses/256/fall2015/QDGL/openwrt-15.05-x86-generic-combined-ext4.img
-
-echo "Unpacking QEMU Binaries"
-gunzip --stdout qemu-install.tar.gz | tar -xvf -
+wget https://www.cs.rochester.edu/courses/256/fall2015/QDGL/openwrt-15.05-x86-generic-combined-ext4.img
