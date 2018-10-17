6a7a02d0330d99c1d27f7ce99907959e02cb2562
An Ho <aho7@cycle1.csug.rochester.edu>
2018-10-17 13:51:47 -0400

    floppy/cs2456/eventstat.c (test file)

--- floppy/cs2456/eventstat.c
+++ floppy/cs2456/eventstat.c
@@ -12,41 +12,26 @@ int main (void) {
     	int *GIDFlag = malloc(sizeof(int));
 
 	int eventID = syscall(181);
-	if(eventID == -1){
-		printf("Error in doeventopen()\n");
-		return 0;
-	}
-	printf("Newly created event with eventID = %d \n", eventID);
+	
+	printf("created eventID = %d \n", eventID);
 	
 	int ret = syscall(214, eventID, UID, GID, UIDFlag, GIDFlag);
-	if(ret == -1){
-		printf("Error in doeventstat()\n");
-		return 0;
-	}
-	printf("Event effective UIDFlag: %d\n",*UIDFlag);
-	printf("Event effective GID: %d\n",*GIDFlag);
+	
+	printf("UIDFlag: %d\n",*UIDFlag);
+	printf("GIDFlag: %d\n",*GIDFlag);
 
-    	printf("Process is root can change everthing.\n");
-	ret = syscall(211, eventID, 0, 0);
-	if(ret == -1){
-		printf("Fail to doeventchown()\n");
-	}
-	else{
-		printf("Successfully change mode of event: %d\n",eventID);
-		printf("Event new effective UID: %d\n",*UID);
-		printf("Event new effective GID: %d\n",*GID);
-        printf("Correct\n");
-	}
+	ret = syscall(211, eventID, 1, 0);
+
+	printf("Root can change mode of event: %d\n",eventID);
+	printf("New UIDFlag: %d\n",*UIDFlag);
+	printf("New GIDFlag: %d\n",*GIDFlag);
 
 	ret = syscall(214, eventID, UID, GID, UIDFlag, GIDFlag);
-	if(ret == -1){
-		printf("Error in doeventstat()\n");
-		return 0;
-	}
-	printf("Event new effective UID: %d\n",*UID);
-	printf("Event new effective GID: %d\n",*GID);
-	printf("Event new effective UIDFlag: %d\n", *UIDFlag);
-	printf("Event new effective GIDFlag: %d\n", *GIDFlag);	
+	
+	printf("EVENTSTAT: UID: %d\n",*UID);
+	printf("GID: %d\n",*GID);
+	printf("UIDFlag: %d\n", *UIDFlag);
+	printf("GIDFlag: %d\n", *GIDFlag);	
 
 	return 0;
 }
